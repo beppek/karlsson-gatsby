95860835cf78045d2746059b19a11ca417399bf7
Beppe Karlsson <beppe@live.se>
2019-01-26 14:41:48 +1100

    Footer at bottom right

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Footer = styled.footer`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: right;
+  padding: 10px 20px;
+`;
+
+export default Footer;
