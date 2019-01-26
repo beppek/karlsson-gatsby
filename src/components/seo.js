9ab5b334c28cc0e50b8e3aa896b3624af40523d4
Beppe Karlsson <beppe@live.se>
2019-01-26 14:46:17 +1100

    Auto format and favicon

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from 'gatsby'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+import { StaticQuery, graphql } from 'gatsby';
 
 function SEO({ description, lang, meta, keywords, title }) {
   return (
@@ -9,7 +9,7 @@ function SEO({ description, lang, meta, keywords, title }) {
       query={detailsQuery}
       render={data => {
         const metaDescription =
-          description || data.site.siteMetadata.description
+          description || data.site.siteMetadata.description;
         return (
           <Helmet
             htmlAttributes={{
@@ -57,21 +57,98 @@ function SEO({ description, lang, meta, keywords, title }) {
                       name: `keywords`,
                       content: keywords.join(`, `),
                     }
-                  : []
+                  : [],
               )
               .concat(meta)}
-          />
-        )
+          >
+            <link
+              rel="apple-touch-icon"
+              sizes="57x57"
+              href="/apple-icon-57x57.png"
+            />
+            <link
+              rel="apple-touch-icon"
+              sizes="60x60"
+              href="/apple-icon-60x60.png"
+            />
+            <link
+              rel="apple-touch-icon"
+              sizes="72x72"
+              href="/apple-icon-72x72.png"
+            />
+            <link
+              rel="apple-touch-icon"
+              sizes="76x76"
+              href="/apple-icon-76x76.png"
+            />
+            <link
+              rel="apple-touch-icon"
+              sizes="114x114"
+              href="/apple-icon-114x114.png"
+            />
+            <link
+              rel="apple-touch-icon"
+              sizes="120x120"
+              href="/apple-icon-120x120.png"
+            />
+            <link
+              rel="apple-touch-icon"
+              sizes="144x144"
+              href="/apple-icon-144x144.png"
+            />
+            <link
+              rel="apple-touch-icon"
+              sizes="152x152"
+              href="/apple-icon-152x152.png"
+            />
+            <link
+              rel="apple-touch-icon"
+              sizes="180x180"
+              href="/apple-icon-180x180.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="192x192"
+              href="/android-icon-192x192.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="32x32"
+              href="/favicon-32x32.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="96x96"
+              href="/favicon-96x96.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="16x16"
+              href="/favicon-16x16.png"
+            />
+            <link rel="manifest" href="/manifest.json" />
+            <meta name="msapplication-TileColor" content="#ffffff" />
+            <meta
+              name="msapplication-TileImage"
+              content="/ms-icon-144x144.png"
+            />
+            <meta name="theme-color" content="#ffffff" />
+          </Helmet>
+        );
       }}
     />
-  )
+  );
 }
 
 SEO.defaultProps = {
   lang: `en`,
   meta: [],
   keywords: [],
-}
+};
 
 SEO.propTypes = {
   description: PropTypes.string,
@@ -79,9 +156,9 @@ SEO.propTypes = {
   meta: PropTypes.array,
   keywords: PropTypes.arrayOf(PropTypes.string),
   title: PropTypes.string.isRequired,
-}
+};
 
-export default SEO
+export default SEO;
 
 const detailsQuery = graphql`
   query DefaultSEOQuery {
@@ -93,4 +170,4 @@ const detailsQuery = graphql`
       }
     }
   }
-`
+`;
